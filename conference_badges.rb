90f5a49c913b9fffb073fa7539c2c4433da47540
Antony Sanders <antony.devon92@gmail.com>
2019-11-12 13:52:27 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,7 +5,7 @@ end
 
 def batch_badge_creator(attendees)
   attendees.each do |name|
-  print "Hello, my name is #{name}."
-  return "Hello, my name is #{name}."
+  "Hello, my name is #{name}."
+  
 end
 end
