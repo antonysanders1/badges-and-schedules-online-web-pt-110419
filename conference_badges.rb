f8cf44219ea136b1a40630c9dfd6a9a744380946
Antony Sanders <antony.devon92@gmail.com>
2019-11-12 19:01:32 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -31,9 +31,9 @@ end
 def printer(attendees)
   badges = batch_badge_creator
   rooms = assign_rooms
-  attendees.each do ||
-    badges
-    rooms
+  attendees.each do |name|
+    puts badges
+    puts rooms
   end
   
 end
